1231a213ccefde901bb3feeaaa238db256a1ccd5
paramov213 <epiksou5@gmail.com>
2026-02-10 22:28:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,21 +3,21 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const fs = require('fs');
-const path = require('path'); // Добавлено
+const path = require('path');
 
-// Раздача статических файлов (твоего HTML)
+const USERS_FILE = './users.json';
+const MSGS_FILE = './messages.json';
+
+// Загрузка данных
+let users = fs.existsSync(USERS_FILE) ? JSON.parse(fs.readFileSync(USERS_FILE)) : [];
+let messages = fs.existsSync(MSGS_FILE) ? JSON.parse(fs.readFileSync(MSGS_FILE)) : [];
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Файлы "базы данных"
-const USERS_FILE = './users.json';
-const MSGS_FILE = './messages.json';
-
-let users = fs.existsSync(USERS_FILE) ? JSON.parse(fs.readFileSync(USERS_FILE)) : [];
-let messages = fs.existsSync(MSGS_FILE) ? JSON.parse(fs.readFileSync(MSGS_FILE)) : [];
-
 io.on('connection', (socket) => {
+    // Синхронизация
     socket.on('request_sync', (username) => {
         socket.emit('sync_data', {
             history: users,
@@ -25,21 +25,32 @@ io.on('connection', (socket) => {
         });
     });
 
+    // Обновление профиля
     socket.on('update_profile_broadcast', (userData) => {
         const idx = users.findIndex(u => u.username === userData.username);
         if (idx !== -1) users[idx] = userData;
         else users.push(userData);
         fs.writeFileSync(USERS_FILE, JSON.stringify(users));
-        io.emit('user_profile_updated', userData);
+        io.emit('sync_data', { history: users, messages: [] }); 
     });
 
+    // Личные сообщения
     socket.on('private_msg', (data) => {
         messages.push(data);
         fs.writeFileSync(MSGS_FILE, JSON.stringify(messages));
-        io.emit('receive_msg', data); // Отправляем всем, клиент сам отфильтрует
+        io.emit('receive_msg', data);
+    });
+
+    // ИСПРАВЛЕННОЕ УДАЛЕНИЕ (Серверная часть)
+    socket.on('adm_delete_user', (username) => {
+        users = users.filter(u => u.username !== username);
+        messages = messages.filter(m => m.from !== username && m.to !== username);
+        fs.writeFileSync(USERS_FILE, JSON.stringify(users));
+        fs.writeFileSync(MSGS_FILE, JSON.stringify(messages));
+        io.emit('user_deleted', username); // Сигнал всем клиентам
     });
 
     socket.on('typing', (data) => socket.broadcast.emit('display_typing', data));
 });
 
-http.listen(3000, () => console.log('Сервер: http://localhost:3000'));
+http.listen(3000, () => console.log('Сервер запущен: http://localhost:3000'));
